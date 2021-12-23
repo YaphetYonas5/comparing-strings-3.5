c399b8033fab54f3987df804b1755f25094da9f4
YaphetYonas1 <85519133+YaphetYonas5@users.noreply.github.com>
2021-12-23 02:53:19 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+   String name1 = "Alan Turing";
+String name2 = "Ada Lovelace";
+//alphebetical order
+//equals and compareTo are what are used for strings.
+//== and >< are used for integers
+if (name1.equals(name2))
+{
+   System.out.println("The names are the same.");
+}
+else
+{
+   System.out.println("The names are not same.");
+}
+
+
+int flag = name1.compareTo(name2);
+
+if (flag == 0)
+{
+   System.out.println("The names are the same.");
+}
+else if (flag < 0)
+{
+   System.out.println("name1 comes before name2.");
+}
+else
+{
+   System.out.println("name2 comes before name1.");
+}
+  }
+}
